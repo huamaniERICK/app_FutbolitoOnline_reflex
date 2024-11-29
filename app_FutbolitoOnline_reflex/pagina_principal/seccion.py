7769de7a7d0ad08c8e5d335f7f80f1b1bee59d80
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2024-11-29 15:47:19 -0500

    actualizo mi pagina

--- app_FutbolitoOnline_reflex/pagina_principal/seccion.py
+++ app_FutbolitoOnline_reflex/pagina_principal/seccion.py
@@ -1,30 +1,28 @@
 import reflex as rx
 
 def seccion():
-    return rx.box(
+    return rx.flex(
         rx.box(
             rx.hstack(
-                rx.html('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_AfkWlpCMOdxqNWi28h3Zv7pI70mciJiWrQ&s"  width="866" height="550">'),
+                rx.html('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_AfkWlpCMOdxqNWi28h3Zv7pI70mciJiWrQ&s"  width="866" height="100%">')),
+                
 
-            ),
-            rx.box(
+        ),
+        rx.box(
+            rx.hstack(
                 rx.text("Mis torneos",size="9",style={"font-family":"quesha"}),
-                rx.vstack(
                 rx.button(
                     "CREAR TORNEOS",
                     color_scheme="blue",
                     on_click=lambda:rx.redirect("/create_page"),
-                    margin_top="40em",
+                    # margin_top="40em",
                     width="30%",
                 ),
-            )
-            
-            
-                width="40%"
-
             ),
-
-            
         ),
     ),
+
+            
+    
+    
     
