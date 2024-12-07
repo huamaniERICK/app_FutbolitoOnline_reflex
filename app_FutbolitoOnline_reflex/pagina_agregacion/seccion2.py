92b7c1abfbc4b8eaf37f937e83d59802fee0d30d
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2024-12-06 21:52:48 -0500

    agrego el componente taps a mi pagina

--- app_FutbolitoOnline_reflex/pagina_agregacion/seccion2.py
+++ app_FutbolitoOnline_reflex/pagina_agregacion/seccion2.py
@@ -3,59 +3,71 @@ import reflex as rx
 def seccion2():
     return rx.box(
         rx.box(
-            rx.vstack(
-                rx.link(
-                    "Generalidades",
-                    color="black",
-                    size="5",
-                    margin_top="3em",
-                    
+            rx.tabs.root(
+                rx.tabs.list(
+                    rx.tabs.trigger("Generalidades", value="tab1"),
+                    rx.tabs.trigger("Participantes", value="tab2"),
+                    rx.tabs.trigger("Clasificacion", value="tab3"),
+                    rx.tabs.trigger("Resultados", value="tab4"),
                 ),
-                rx.link(
-                    "Participantes",
-                    color="black",
-                    size="5",
-                    
+                rx.tabs.content(
+                    rx.box(
+                        rx.center(
+                            rx.vstack(
+                                rx.text("Nombre del torneo",size="7",style={"font-family":"quesha"}),
+                                rx.hstack(
+                                    rx.input(placeholder="Coloca un nombre",width="40em",id="coloca un nombre"),
+                                    rx.button("Cambiar",on_click=rx.set_value("coloca un nombre","")),
+                                    margin_top="-1em",
+                                    margin_bottom="2em",
+                                ),
+                                rx.text("Ubicacion",size="7",style={"font-family":"quesha"}),
+                                rx.hstack(
+                                    rx.input(placeholder="Ingresa una ubicacion...",width="40em",id="Ingresa una ubicacion..."),
+                                    rx.button("Cambiar",on_click=rx.set_value("Ingresa una ubicacion..","")),
+                                    margin_top="-1em",
+                                    margin_bottom="2em",
+                                ),
+                                rx.text("Fecha del torneo",size="7",style={"font-family":"quesha"}),
+                                rx.hstack(
+                                    rx.input(placeholder="DD/MM/AA",width="40em",id="DD/MM/AA"),
+                                    rx.button("Cambiar",on_click=rx.set_value("DD/MM/AA","")),
+                                ),
+                                margin_top="2em",
+                            ),
+                        ),
+                    ),
+                    value="tab1",
                 ),
-                rx.link(
-                    "Clasificacion",
-                    color="black",
-                    size="5",
-                    
+                rx.tabs.content(
+                    rx.text("item on tab 2"),
+                    value="tab2",
                 ),
-                rx.link(
-                    "Resultados",
-                    color="black",
-                    size="5",
-                    
+                rx.tabs.content(
+                    rx.box(
+                        rx.center(
+                            rx.vstack(
+                                rx.text("Elige la clasificacion del torneo",size="7",style={"font-family":"quesha"},margin_left="11em"),
+                                rx.hstack(
+                                    rx.text("Etapa grupal",size="5",style={"font-family":"arial black"},margin_right="8em"),
+                                    rx.text("Etapa grupal y Etapa Eliminatoria",size="5",style={"font-family":"arial black"},margin_right="8em"),
+                                    rx.text("Etapa Eliminatoria",size="5",style={"font-family":"arial black"}),
+                                    margin_top="3em",
+                                ),
+                            ),
+                            margin_top="2em",
+                        ),
+                    ),
+                    value="tab3",
                 ),
-                
-                align_items="center",
-                justify_content="center",
-                spacing="9",
-                style={"font-family":"quesha"},
-            
+                rx.tabs.content(
+                    rx.text("item on tab 4"),
+                    value="tab4",
+                ),
+                default_value="tab1",
+                orientation="vertical",
             ),
-            background="#0fc3f0",
-            width="10%",
-            height="89vh",
-            
-        
-        
         ),
-    rx.box(
-        rx.center(
-            rx.vstack(
-                rx.text("Nombre del torneo",size="5",margin_top="-32em",align="center"),
-                rx.hstack(
-                    rx.input(id="input1",width="450px"),
-                    rx.button("Cancelar", on_click=rx.set_value("input1",""),),
-                ),
-            ),
+    ),
+      
             
-        ),
-        
-    ),
-
-    ),
-        
